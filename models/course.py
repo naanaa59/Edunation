949320647545535993d5361327a4fd87dae9f7b0
Badr_Annabi <badrannabi@gmail.com>
2024-05-02 15:42:18 +0100

    add last changes in course

--- models/course.py
+++ models/course.py
@@ -11,21 +11,26 @@ from sqlalchemy.orm import relationship
 class Course(Base, BaseDB):
     """ Course Class definition """
     __tablename__ = 'courses'
-    subject_id = Column(String(60), ForeignKey('subjects.id'), nullable=False)
-    student_id = Column(String(60), ForeignKey('students.id'), nullable=True)
-    instructor_id = Column(String(60), ForeignKey('instructors.id'), nullable=False)
-    link_photo = Column(String(1000), nullable=True)
+    __mapper_args__ = {
+        'confirm_deleted_rows': False
+    }
+    subject_id = Column(String(60), ForeignKey('subjects.id'), nullable=True)
+    # student_id = Column(String(60), ForeignKey('students.id', ondelete='SET NULL'), nullable=True)
+    instructor_id = Column(String(60), ForeignKey('instructors.id'), nullable=True)
+
     title = Column(String(128), nullable=True)
     # link_video = Column(String(1000), nullable=True)
+    link_photo = Column(String(1000), nullable=True)
     description = Column(String(15000), nullable=True)
+
     subject = relationship("Subject", back_populates="courses")
-    # students = relationship("Student", secondary="StudentCourses",
-    #                         viewonly=False, back_populates="courses")
-    student_courses = relationship("StudentCourses", back_populates="courses")
-    # instructors = relationship("Instructor", secondary="instructor_courses",
-    #                            viewonly=False, back_populates="courses")
-    instructor_courses = relationship("InstructorCourses", back_populates="courses")
+    
+    student_courses = relationship("StudentCourses", back_populates="courses",
+                                   cascade="all, delete")
+
+    instructor_courses = relationship("InstructorCourses", back_populates="courses",
+                                      cascade="all, delete")
 
     def __init__(self, *args, **kwargs):
-        """ initializes user """
+        """ initializes course """
         super().__init__(*args, **kwargs)
