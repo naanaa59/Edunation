28ada7bb67dc6c2c45527a2a2fae30093de17a16
Badr Annabi <badrannabi@Badrs-MacBook-Pro.local>
2024-05-05 00:03:57 +0100

    credential

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -16,21 +16,21 @@ const HeroSection = () => {
                 
                 <div className='flex gap-6'>
                 <div className='lg:max-w'>
-                    <a href="/about" class="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 text-white">
+                    <a href="/about" class="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 ">
                     <span className="absolute inset-0 bg-indigo-600 transition-all duration-500 ease-in-out transform origin-left scale-x-0 group-hover:scale-x-100"></span>
                         <span class="relative text-indigo-600 transition duration-300 group-hover:text-white ease tracking-wider">About Us</span>
                         
                     </a>
                 </div>
                 <div className='lg:max-w'>
-                    <a href="#_" class="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 text-white">
+                    <a href="/login" class="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 ">
                     <span className="absolute inset-0 bg-indigo-600 transition-all duration-500 ease-in-out transform origin-left scale-x-0 group-hover:scale-x-100"></span>
                         <span class="relative text-indigo-600 transition duration-300  group-hover:text-white ease tracking-wider "> Learner</span>
                         
                     </a>
                 </div>
                 <div className='lg:max-w'>
-                    <a href="#_" class="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 text-white">
+                    <a href="/login" class="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 ">
                     <span className="absolute inset-0 bg-indigo-600 transition-all duration-500 ease-in-out transform origin-left scale-x-0 group-hover:scale-x-100"></span>
                         <span class="relative text-indigo-600 transition duration-300 group-hover:text-white ease tracking-wider">Instructor</span>
                         
--- client/src/scenes/LoginPage.jsx
+++ client/src/scenes/LoginPage.jsx
@@ -1,16 +1,102 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { GoogleLogin } from '@react-oauth/google'
+import { jwtDecode } from 'jwt-decode'
+import { useCookies } from 'react-cookie'
+
 
 const LoginPage = () => {
+  const responseMessage = (credentialResponse) => {
+    const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
+    console.log(credentialResponseDecoded);
+  };
+
+  const errorMessage = (error) => {
+    console.log(error);
+  };
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasworError] = useState('');
+  const [cookies, setCookie] = useCookies(['user']);
+  const navigate = useNavigate();
+
+  const loginUser = async (email, password) => {
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      if (response.ok) {
+        const result = await response.json();
+        if (result.success) {
+          setCookie('user', email, { path: '/' });
+          navigate('/dashboard'); // Navigate to the dashboard or desired page after login
+        } else {
+          // Handle login failure
+          console.error('Login failed');
+        }
+      } else {
+        // Handle network error
+        console.error('Network error');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
+  const onButtonClick = (e) => {
+    e.preventDefault();
+    if (email && password) {
+      loginUser(email, password);
+    } else {
+      setEmailError('Email is required')
+      setPasworError('Password is required')
+    }
+  }
   return (
-    <div>
-        <Navbar />
-        <form className='h-screen' action="">
-            
-        </form>
-        <Footer />
+    <div className='flex'>
+      <div className='bg-indigo-600 w-1/2 flex justify-center items-center'>
+        <p className='gothic text-4xl text-center text-white'>You're one step close to be part of our community</p>
+      </div>
+      <div className='w-1/2 flex flex-col items-center justify-center h-screen'>
+        <div className='bg-red-300 '>
+          <div className='gothic text-2xl'>Login</div>
+        </div>
+        <br />
+        <div className='my-4'>
+          <GoogleLogin onSuccess={responseMessage}  onError={errorMessage} />
+        </div>
+        <div className='flex flex-col'>
+          <input 
+            value={email}
+            placeholder='Enter your email'
+            onChange={(e) => setEmail(e.target.value)}
+            className='inputBox'
+            />
+            <label className='errorLabel'>{emailError}</label>
+        </div>
+        <br />
+        <div className='flex flex-col'>
+        <input 
+            value={password}
+            placeholder='Enter your password'
+            onChange={(e) => setPassword(e.target.value)}
+            className='inputBox'
+            />
+            <label className='errorLabel'>{passwordError}</label>
+        </div>
+        <br />
+        <div>
+          <input className="bg-indigo-600" type="button" onClick={onButtonClick} value={'Log in'} />
+        </div>
     </div>
+    </div>
+    
   )
 }
 
