9114ab06e087eac9a13d14a11d8d192f5d8cc02b
Badr_Annabi <badrannabi@gmail.com>
2024-05-10 17:20:17 +0100

    add enroll func but has failed to add student (AttributeError: 'Course' object has no attribute 'students')

--- client/src/scenes/CoursesPage.jsx
+++ client/src/scenes/CoursesPage.jsx
@@ -4,53 +4,90 @@ import Footer from '../components/Footer'
 import coursepic from '../images/infocourse.jpeg'
 import { useNavigate,  useParams } from 'react-router-dom'
 
+
+
 const CousesPage = () => {
   const [courseInfo, setCourseInfo] = useState([]);
+  // const [userInfo, setUserInfo] = useState([]);
   const { courseId } = useParams();
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    const check_token = async () => {
+    const checkToken = async () => {
       await new Promise(resolve => setTimeout(resolve, 1000));
-      const token =  localStorage.getItem("access_token")
-     if (token) {
-      const result = await fetch('http://0.0.0.0:5003/token_check/',
-        {
-          method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-            }
-        }
-      )
-      const courseResponse = await fetch(`http://0.0.0.0:5003/courses/${courseId}`,
-        {
+      const token = localStorage.getItem("access_token");
+      if (token) {
+        const result = await fetch('http://0.0.0.0:5003/token_check/', {
           method: 'GET',
           headers: {
-            'Authorization':`Bearer ${token}`,
-          
+            'Authorization': `Bearer ${token}`,
+          }
+        });
+        const courseResponse = await fetch(`http://0.0.0.0:5003/courses/${courseId}`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
           },
         });
-      if (result.ok) {
-        console.log("ok")
-        console.log("result:", result);
-        console.log("CourseResponse:", courseResponse)
-        const course = await courseResponse.json();
-        console.log("course info: ", course);      
-        setCourseInfo(course); 
-      } else {
-        navigate('/login')
-        console.log("Test")
+        if (result.ok) {
+          console.log("ok");
+          console.log("result:", result);
+          // const userInfo = await result.json();
+          // setUserInfo(userInfo.user);
+          console.log("CourseResponse:", courseResponse);
+          const course = await courseResponse.json();
+          console.log("course info: ", course);
+          setCourseInfo(course);
+        } else {
+          navigate('/login');
+          console.log("Test");
+        }
+        console.log(result);
       }
-       console.log(result)
-       
-    }
-      }
-check_token();  
-}, [navigate, courseId]) 
+    };
+    checkToken();
+  }, [navigate, courseId]);
 
-if (!courseInfo) {
-  return <div>Loading...</div>
-}
+
+  const enrollStudent = async () => {
+    try {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const token = localStorage.getItem("access_token");
+      if (token) {
+        const result = await fetch('http://0.0.0.0:5003/token_check/', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          }
+        });
+      
+      const userInfo = await result.json();
+      // setUserInfo(userInfo.user);
+      const student = userInfo.user;
+      const enrollResponse = await fetch(`http://0.0.0.0:5003/courses/${courseId}/enroll/${student.id}`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+      if (!enrollResponse.ok) {
+        throw new Error('Failed to enroll student');
+      }
+      const data = await enrollResponse.json();
+      console.log("enroll data", data.message);
+    } 
+    } catch (error) {
+      console.log(error);
+      // navigate('/404'); 
+    }
+  };
+
+  
+
+  if (!courseInfo) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
@@ -60,11 +97,10 @@ if (!courseInfo) {
         <p className='pt-20'>{courseInfo.title}</p>
         <p>{courseInfo.description}</p>
         <div className='lg:max-w flex '>
-                    <a href="/" class="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 ">
-                    <span className="absolute inset-0 bg-indigo-600 transition-all duration-500 ease-in-out transform origin-left scale-x-0 group-hover:scale-x-100"></span>
-                        <span class="relative text-indigo-600 transition duration-300 group-hover:text-white ease tracking-wider"> Enroll </span>
-                        
-                    </a>
+        <button onClick={() => enrollStudent(1)} className="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600">
+  <span className="absolute inset-0 bg-indigo-600 transition-all duration-500 ease-in-out transform origin-left scale-x-0 group-hover:scale-x-100"></span>
+  <span className="relative text-indigo-600 transition duration-300 group-hover:text-white ease tracking-wider"> Enroll </span>
+</button>
                 </div>
       </div>
       
